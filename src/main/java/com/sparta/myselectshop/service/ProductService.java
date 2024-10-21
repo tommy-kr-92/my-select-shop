a4a4fa5e85b92dbab1abae6c7129852dade373da
tommyheo <h01041303675@gmail.com>
2024-10-21 10:11:21 +0900

    상품 페이징 및 정렬

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -5,9 +5,14 @@ import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.entity.User;
+import com.sparta.myselectshop.entity.UserRoleEnum;
 import com.sparta.myselectshop.naver.dto.ItemDto;
 import com.sparta.myselectshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -44,15 +49,25 @@ public class ProductService {
 		return new ProductResponseDto(product);
 	}
 
-	public List<ProductResponseDto> getProducts(User user) {
-		List<Product> productList = productRepository.findAllByUser(user);
-		List<ProductResponseDto> responseDtoList = new ArrayList<>();
+	public Page<ProductResponseDto> getProducts(User user, int page, int size, String sortBy, boolean isAsc) {
+		// 정렬 전처리
+		Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+		Sort sort = Sort.by(direction, sortBy);
+		Pageable pageable = PageRequest.of(page, size, sort);
 
-		for (Product product : productList) {
-				responseDtoList.add(new ProductResponseDto(product));
+		UserRoleEnum userRoleEnum = user.getRole();
+
+		Page<Product> productList;
+
+		// 권한에 따른 다른 메서드 호출
+		if(userRoleEnum == UserRoleEnum.USER){
+			productList = productRepository.findAllByUser(user, pageable);
+		} else {
+			productList = productRepository.findAll(pageable);
 		}
-
-		return responseDtoList;
+		
+		// Page 타입은 converter 인 map 메서드 지원
+		return productList.map(ProductResponseDto::new);
 	}
 
 	@Transactional
@@ -61,16 +76,5 @@ public class ProductService {
 				new NullPointerException("해당 상품은 존재하지 않습니다.")
 				);
 		product.updateByItemDto(itemDto);
-	}
-
-	public List<ProductResponseDto> getAllProducts() {
-		List<Product> productList = productRepository.findAll();
-		List<ProductResponseDto> responseDtoList = new ArrayList<>();
-
-		for (Product product : productList) {
-			responseDtoList.add(new ProductResponseDto(product));
-		}
-
-		return responseDtoList;
 	}
 }
