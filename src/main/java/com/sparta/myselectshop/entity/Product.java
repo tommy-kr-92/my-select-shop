190a8963757dd3c7e4aecb6b1be8aa0b3d8e731c
tommyheo <h01041303675@gmail.com>
2024-10-21 09:49:24 +0900

    회원별 상품 API 구현

--- src/main/java/com/sparta/myselectshop/entity/Product.java
+++ src/main/java/com/sparta/myselectshop/entity/Product.java
@@ -34,11 +34,17 @@ public class Product extends Timestamped {
 	@Column(nullable = false)
 	private int myprice;
 
-	public Product(ProductRequestDto requestDto) {
+	// 상품과 유저의 관계설정 - 다대일 단방향
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	public Product(ProductRequestDto requestDto, User user) {
 		this.title = requestDto.getTitle();
 		this.image = requestDto.getImage();
 		this.link = requestDto.getLink();
 		this.lprice = requestDto.getLprice();
+		this.user = user;
 	}
 
 	public void update(ProductMypriceRequestDto requestDto) {
